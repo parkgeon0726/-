faa9da589af8a971b513ea4ef54cffe9cbb759ae
parkgeon0726 <hit102412@naver.com>
2025-03-25 12:08:16 +0900

    3.25기초문제풀이

---
+++ 기초문제.py
@@ -0,0 +1,25 @@
+#3.25 출력관련문제풀이 (새싹문제풀이임)
+
+#https://www.acmicpc.net/problem/2557 문제
+#답 : print('Hello World!')
+
+#https://www.acmicpc.net/problem/10699 문제
+#답 : from datetime import datetime
+#답 : print(datetime.now().strftime("%Y-%m-%d"))
+
+#https://www.acmicpc.net/problem/7287 문제
+#답 : print("2")
+#답 : print("hit20200405")
+
+#https://www.acmicpc.net/problem/10171 문제
+#답 : print("\\    /\\")
+#답 : print(" )  ( ')")
+#답 : print("(  /  )")
+#답 : print(" \\(__)|")
+
+#https://www.acmicpc.net/problem/10172 문제
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
